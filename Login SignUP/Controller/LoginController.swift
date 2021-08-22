0867bdf480b9d8e2f8a662ce20bfabb5404b1e45
Joon <kjo11300@naver.com>
2021-08-22 15:13:26 +0900

    이메일, 비밀번호 textfield 생성

--- Login SignUP/Controller/LoginController.swift
+++ Login SignUP/Controller/LoginController.swift
@@ -10,6 +10,43 @@ import UIKit
 class LoginController: UIViewController {
     
     private let iconImage = UIImageView(image: #imageLiteral(resourceName: "firebase-logo"))
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        tf.leftView = spacer
+        tf.leftViewMode = .always
+        
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.setHeight(height: 50)
+        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor:UIColor(white: 1.0, alpha: 0.5)])
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        tf.leftView = spacer
+        tf.leftViewMode = .always
+        
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.isSecureTextEntry = true
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.setHeight(height: 50)
+        
+        tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor:UIColor(white: 1.0, alpha: 0.5)])
+        return tf
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +68,14 @@ class LoginController: UIViewController {
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 120, width: 120)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top : iconImage.bottomAnchor, left : view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32, paddingLeft: 32,
+                     paddingRight: 32)
     }
 }
